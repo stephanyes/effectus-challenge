ef8820c7af76b2f5ccc54c6ce399498a66c38a33
stephanyes <stephanoiucciolino@gmail.com>
2022-12-31 13:55:59 -0300

    removed unused paths

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import * as controller from "../controller/controller";
 
 const router = Router();
-// TODO -> Separate the CRUD routes from the "special ones" so that we can differenciate them
+
 // Campground
 router.get("/campgrounds", controller.getAllCampgrounds);
 router.get("/campgrounds/order", controller.getCampgroundsByPrice);
@@ -14,10 +14,6 @@ router.delete("/campgrounds/:id", controller.deleteCampground);
 // Campsite
 router.get("/campsites", controller.getAllCampsites);
 router.get("/campsites/:id", controller.getCampsiteById);
-router.get(
-  "/campsites/:campsiteId/bookings",
-  controller.getBookingsByCampsiteAndDates
-);
 router.post("/campsites", controller.createCampsite);
 router.put("/campsites/:id", controller.updateCampsite);
 router.delete("/campsites/:id", controller.deleteCampsite);
@@ -27,7 +23,6 @@ router.get(
   "/bookings/campsites/:campsiteId/dates/:startDate/:endDate",
   controller.getBookingsByCampsiteAndDates
 );
-router.get("/campsite/:id/bookings", controller.getBookingsByCampsiteId);
 router.post("/bookings", controller.createBooking);
 
 export default router;
